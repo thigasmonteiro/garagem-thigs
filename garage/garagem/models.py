a996e8340b1f83afb8e9c35a5092ab1e71ac380e
thigas.monteiro <thiagodepaulamonteiro@gmail.com>
2023-08-31 22:43:49 -0300

    Criação de Modelo Categoria

--- garage/garagem/models.py
+++ garage/garagem/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, null=True, blank=True) 
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True) #lembrar
     
     def __str__(self):
         return self.nome.upper() 
